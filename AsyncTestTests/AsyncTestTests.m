f186ba6ed37b04c260d8f26bc49fdf3f84e2f079
Sven A. Schmidt <sas@abstracture.de>
2012-11-14 12:14:40 +0000

    Example test case

--- AsyncTestTests/AsyncTestTests.m
+++ AsyncTestTests/AsyncTestTests.m
@@ -7,26 +7,73 @@
 //
 
 #import "AsyncTestTests.h"
+#import "SenTestCase+Async.h"
+
+
+@interface Downloader : NSObject
+- (void)startDownload;
+@end
+
+@implementation Downloader
+
+
+- (void)startDownload
+{
+  [self performSelector:@selector(sendNotification) withObject:nil afterDelay:1];
+}
+
+
+- (void)startDownloadWithCompletion:(void (^)())completion
+{
+  NSTimeInterval delay = 1;
+  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delay * NSEC_PER_SEC), dispatch_get_current_queue(), completion);
+}
+
+
+- (void)sendNotification
+{
+  [[NSNotificationCenter defaultCenter] postNotificationName:@"DownloadCompleted" object:self];
+}
+
+@end
+
+
 
 @implementation AsyncTestTests
 
-- (void)setUp
+
+- (void)test_notification
 {
-    [super setUp];
-    
-    // Set-up code here.
+  Downloader *dl = [[Downloader alloc] init];
+  
+  __block BOOL received = NO;
+  [[NSNotificationCenter defaultCenter] addObserverForName:@"DownloadCompleted" object:dl queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
+    received = YES;
+  }];
+  
+  [dl startDownload];
+  
+  [self waitWithTimeout:1.1 forSuccessInBlock:^BOOL{
+    return received;
+  }];
+  STAssertTrue(received, nil);
 }
 
-- (void)tearDown
+
+- (void)test_completion
 {
-    // Tear-down code here.
-    
-    [super tearDown];
+  Downloader *dl = [[Downloader alloc] init];
+  
+  __block BOOL received = NO;
+  [dl startDownloadWithCompletion:^{
+    received = YES;
+  }];
+  
+  [self waitWithTimeout:1.1 forSuccessInBlock:^BOOL{
+    return received;
+  }];
+  STAssertTrue(received, nil);
 }
 
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in AsyncTestTests");
-}
 
 @end
